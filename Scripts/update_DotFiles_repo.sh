e67b7af001388f5cec47e948402e3a2b55ef0706
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2023-08-06 22:38:08 +0300

    updating and adding files

--- Scripts/update_DotFiles_repo.sh
+++ Scripts/update_DotFiles_repo.sh
@@ -1,4 +1,5 @@
 cp  ~/.Xresources   ~/Disk_D/Muhammad/Repositories/DotFiles/
+cp  ~/.gtkrc-2.0    ~/Disk_D/Muhammad/Repositories/DotFiles/
 cp  ~/.hidden       ~/Disk_D/Muhammad/Repositories/DotFiles/
 cp  ~/.ideavimrc    ~/Disk_D/Muhammad/Repositories/DotFiles/
 cp  ~/.inputrc      ~/Disk_D/Muhammad/Repositories/DotFiles/
@@ -8,10 +9,12 @@ cp  ~/.xprofile     ~/Disk_D/Muhammad/Repositories/DotFiles/
 cp  ~/.zshrc        ~/Disk_D/Muhammad/Repositories/DotFiles/
 
 cp  -r  ~/.fonts               ~/Disk_D/Muhammad/Repositories/DotFiles/
-# cp  -r  ~/.themes              ~/Disk_D/Muhammad/Repositories/DotFiles/
 # cp  -r  ~/.icons               ~/Disk_D/Muhammad/Repositories/DotFiles/
+# cp  -r  ~/.themes              ~/Disk_D/Muhammad/Repositories/DotFiles/
 cp  -r  ~/Scripts              ~/Disk_D/Muhammad/Repositories/DotFiles/
 
+cp  -r  ~/.config/Kvantum      ~/Disk_D/Muhammad/Repositories/DotFiles/.config/
+cp  -r  ~/.config/Thunar       ~/Disk_D/Muhammad/Repositories/DotFiles/.config/
 cp  -r  ~/.config/alacritty    ~/Disk_D/Muhammad/Repositories/DotFiles/.config/
 cp  -r  ~/.config/dunst        ~/Disk_D/Muhammad/Repositories/DotFiles/.config/
 cp  -r  ~/.config/fontconfig   ~/Disk_D/Muhammad/Repositories/DotFiles/.config/
@@ -20,11 +23,11 @@ cp  -r  ~/.config/gtk-3.0      ~/Disk_D/Muhammad/Repositories/DotFiles/.config/
 cp  -r  ~/.config/gtk-4.0      ~/Disk_D/Muhammad/Repositories/DotFiles/.config/
 cp  -r  ~/.config/i3           ~/Disk_D/Muhammad/Repositories/DotFiles/.config/
 cp  -r  ~/.config/kitty        ~/Disk_D/Muhammad/Repositories/DotFiles/.config/
-cp  -r  ~/.config/Kvantum      ~/Disk_D/Muhammad/Repositories/DotFiles/.config/
 cp  -r  ~/.config/mpv          ~/Disk_D/Muhammad/Repositories/DotFiles/.config/
 cp  -r  ~/.config/newsboat     ~/Disk_D/Muhammad/Repositories/DotFiles/.config/
 cp  -r  ~/.config/nomacs       ~/Disk_D/Muhammad/Repositories/DotFiles/.config/
 cp  -r  ~/.config/nvim         ~/Disk_D/Muhammad/Repositories/DotFiles/.config/
+cp  -r  ~/.config/octave       ~/Disk_D/Muhammad/Repositories/DotFiles/.config/
 cp  -r  ~/.config/picom        ~/Disk_D/Muhammad/Repositories/DotFiles/.config/
 cp  -r  ~/.config/polybar      ~/Disk_D/Muhammad/Repositories/DotFiles/.config/
 cp  -r  ~/.config/qtile        ~/Disk_D/Muhammad/Repositories/DotFiles/.config/
--- Scripts/volume_control/volume_subscribe.sh
+++ Scripts/volume_control/volume_subscribe.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 MAX_VOL=150
-OLD_VOL=$(($(amixer get Master | grep -Po "[0-9]+(?=%)" | tail -1)*100/$MAX_VOL))
+OLD_VOL=$(($(pactl list sinks | grep '^[[:space:]]Volume:' | awk '{print $5}' | tail -1 | sed 's/%//')*100/$MAX_VOL))
 pactl subscribe | grep --line-buffered "sink" |
 while read; do
-	VOL=$(($(amixer get Master | grep -Po "[0-9]+(?=%)" | tail -1)*100/$MAX_VOL))
+	VOL=$(($(pactl list sinks | grep '^[[:space:]]Volume:' | awk '{print $5}' | tail -1 | sed 's/%//')*100/$MAX_VOL))
     if [[ $VOL != $OLD_VOL ]] && [[ $VOL -ne 0 ]]; then
+      echo $VOL
 		volnoti-show $VOL
 		OLD_VOL=$VOL
     elif [[ $VOL != $OLD_VOL ]] && [[ $Vol -eq 0 ]]; then
