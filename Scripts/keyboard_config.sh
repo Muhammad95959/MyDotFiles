0d6ed5311695f8fdbe3c5106f3d23db0d39b82d4
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2023-08-16 19:22:57 +0300

    updating files

--- Scripts/keyboard_config.sh
+++ Scripts/keyboard_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-setxkbmap -layout "us,ara" -option "lv3:ralt_alt,grp:alt_shift_toggle" -option ctrl:nocaps -option ctrl:swap_rwin_rctl
-# setxkbmap -option ctrl:nocaps -option ctrl:swap_rwin_rctl
+# setxkbmap -layout "us,ara" -option "lv3:ralt_alt,grp:alt_shift_toggle" -option ctrl:nocaps -option ctrl:swap_rwin_rctl
+setxkbmap -layout "us,ara" -option "lv3:ralt_alt,grp:alt_shift_toggle" -option ctrl:nocaps
 xcape -e "Control_L=Escape"
 xset r rate 420 42
