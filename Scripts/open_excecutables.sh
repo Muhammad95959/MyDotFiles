c9d6f242428a145465ed5b83f3332e930c9b4174
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2023-08-26 03:59:15 +0300

    updating files

--- Scripts/open_excecutables.sh
+++ Scripts/open_excecutables.sh
@@ -2,20 +2,22 @@
 
 chosen=$(printf "\
 Change Borders' Colors
-Change Nvidia Power Mode
 Open English Learner
-Open Video Downloader
 Toggle Gaps
+Toggle Smart Gaps
 Toggle Polybar Location
-Update MyDotFiles Repo\
+Update MyDotFiles Repo
+Stream Youtube On MPV
+Open Video Downloader\
 " | rofi -dmenu -i -theme ~/.config/rofi/script_chooser.rasi -p "choose a script: ")
 
 case "$chosen" in
     "Change Borders' Colors") bash ~/Scripts/i3_border_changer.sh ;;
-    "Change Nvidia Power Mode") bash ~/Scripts/nvidia_power_mode.sh ;;
     "Open English Learner") kitty -e bash /mnt/Disk_D/Muhammad/English_Learner/english_learner.sh ;;
-    "Open Video Downloader") i3-msg workspace 10; kitty --class yt-dlp -e bash ~/Scripts/yt-dlp_script.sh ;;
     "Toggle Gaps") bash ~/Scripts/toggle_gaps.sh ;;
+    "Toggle Smart Gaps") bash ~/Scripts/toggle_i3_smart_gaps.sh ;;
     "Toggle Polybar Location") bash ~/Scripts/toggle_polybar_location.sh ;;
     "Update MyDotFiles Repo") bash ~/Scripts/update_MyDotFiles_repo.sh ;;
+    "Stream Youtube On MPV") bash ~/Scripts/url_to_mpv.sh ;;
+    "Open Video Downloader") i3-msg workspace 10; kitty --class yt-dlp -e bash ~/Scripts/yt-dlp_script.sh ;;
 esac
