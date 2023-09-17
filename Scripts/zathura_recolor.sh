b7303d8907f11aac01dbc7d7b03205a8de18e7aa
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2023-09-17 06:35:50 +0300

    getting rid of some old files

--- Scripts/zathura_recolor.sh
+++ Scripts/zathura_recolor.sh
@@ -1,37 +1,12 @@
 chosen=$(printf "\
-Canary Yellow on Black
-Yellow on Dark Gray
-Amber on Charcoal
 Mustard on Deep Charcoal
+Canary Yellow on Black
+White on Black
 Black on White
 Black on Beige\
 " | rofi -dmenu -i -theme ~/.config/rofi/zathura_recolor.rasi -p "choose a theme: ")
 
 case "$chosen" in
-"Canary Yellow on Black")
-	sed -i "/-- Recoloring/,+5c\
--- Recoloring\n\
-set default-bg         \"#000000\"\n\
-set statusbar-bg       \"#141414\"\n\
-set recolor-darkcolor  \"#ffee00\"\n\
-set recolor-lightcolor \"#000000\"\n" "$HOME"/.config/zathura/zathurarc
-	;;
-"Yellow on Dark Gray")
-	sed -i "/-- Recoloring/,+5c\
--- Recoloring\n\
-set default-bg         \"#333333\"\n\
-set statusbar-bg       \"#000000\"\n\
-set recolor-darkcolor  \"#ffff00\"\n\
-set recolor-lightcolor \"#333333\"\n" "$HOME"/.config/zathura/zathurarc
-	;;
-"Amber on Charcoal")
-	sed -i "/-- Recoloring/,+5c\
--- Recoloring\n\
-set default-bg         \"#222222\"\n\
-set statusbar-bg       \"#000000\"\n\
-set recolor-darkcolor  \"#ffbf00\"\n\
-set recolor-lightcolor \"#222222\"\n" "$HOME"/.config/zathura/zathurarc
-	;;
 "Mustard on Deep Charcoal")
 	sed -i "/-- Recoloring/,+5c\
 -- Recoloring\n\
@@ -40,13 +15,21 @@ set statusbar-bg       \"#000000\"\n\
 set recolor-darkcolor  \"#ffdb58\"\n\
 set recolor-lightcolor \"#111111\"\n" "$HOME"/.config/zathura/zathurarc
 	;;
-"Black on Beige")
+"Canary Yellow on Black")
 	sed -i "/-- Recoloring/,+5c\
 -- Recoloring\n\
 set default-bg         \"#000000\"\n\
 set statusbar-bg       \"#141414\"\n\
+set recolor-darkcolor  \"#ffee00\"\n\
+set recolor-lightcolor \"#000000\"\n" "$HOME"/.config/zathura/zathurarc
+	;;
+"White on Black")
+	sed -i "/-- Recoloring/,+5c\
+-- Recoloring\n\
+set default-bg         \"#ffffff\"\n\
+set statusbar-bg       \"#000000\"\n\
 set recolor-darkcolor  \"#000000\"\n\
-set recolor-lightcolor \"#ffe1ba\"\n" "$HOME"/.config/zathura/zathurarc
+set recolor-lightcolor \"#ffffff\"\n" "$HOME"/.config/zathura/zathurarc
 	;;
 "Black on White")
 	sed -i "/-- Recoloring/,+5c\
@@ -56,4 +39,24 @@ set statusbar-bg       \"#141414\"\n\
 set recolor-darkcolor  \"#000000\"\n\
 set recolor-lightcolor \"#ffffff\"\n" "$HOME"/.config/zathura/zathurarc
 	;;
+"Black on Beige")
+	sed -i "/-- Recoloring/,+5c\
+-- Recoloring\n\
+set default-bg         \"#000000\"\n\
+set statusbar-bg       \"#141414\"\n\
+set recolor-darkcolor  \"#000000\"\n\
+set recolor-lightcolor \"#ffe1ba\"\n" "$HOME"/.config/zathura/zathurarc
+	;;
 esac
+
+if [ "$(xdotool getactivewindow getwindowclassname)" == "Zathura" ]; then
+  xdotool type ":"
+  xdotool type "s"
+  xdotool type "o"
+  xdotool type "u"
+  xdotool type "r"
+  xdotool type "c"
+  xdotool type "e"
+  xdotool key Return
+  xdotool key Escape
+fi
