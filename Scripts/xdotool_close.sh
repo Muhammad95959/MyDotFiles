d96de9fc315729e16664feae01451cf2bd01b914
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2023-10-30 13:13:58 +0200

    update files

--- Scripts/xdotool_close.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# window_class_name="$(xdotool getactivewindow getwindowclassname)"
-# case $window_class_name in 
-#     "jetbrains-studio" | "jetbrains-idea-ce") xdotool mousemove 1885 978 click 1 sleep 0.02 mousemove restore ;;
-#     "Brave-browser") xdotool mousemove 1890 153 click 1 sleep 0.02 mousemove restore ;;
-# esac
-
-xdotool mousemove 1890 153 click 1 sleep 0.02 mousemove restore
